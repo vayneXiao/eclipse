9029143d1ceb9c28b93d0bef2fc02a4b45daa745
vayneXiao <34128693+vayneXiao@users.noreply.github.com>
2019-10-28 04:03:28 -0700

    Update User.java

--- test-eclipse-pro01/src/com/siki/wuhai/User.java
+++ test-eclipse-pro01/src/com/siki/wuhai/User.java
@@ -3,7 +3,7 @@ package com.siki.wuhai;
 public class User {
 
 	public static void main(String[] args) {
-		 System.out.print("hello world 1111111111111111122222222222222222222");
+		 System.out.print("789");
 	}
 
 }
